9f92f99450f9a84b032ed6565013ee8beb60e743
Mh-Monzil <monzil246@gmail.com>
2024-09-07 13:21:16 +0600

    add: message controller

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json())
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes)
 
 app.listen(PORT, () => {
   connectToMongoDB();
