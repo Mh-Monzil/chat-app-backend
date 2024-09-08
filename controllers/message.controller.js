857330b0fb2316fe6d00dc8d3ef20ae078343cb6
Mh-Monzil <monzil246@gmail.com>
2024-09-09 00:44:15 +0600

    add get message

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -35,3 +35,20 @@ export const sendMessage = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+export const getMessage = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+
+    if (!conversation) return res.status(200).json([]);
+
+    res.status(200).json(conversation.messages);
+  } catch (error) {
+    console.log("Error in sendMessage controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { sendMessage } from "../controllers/message.controller.js";
+import { getMessage, sendMessage } from "../controllers/message.controller.js";
 import verifyJWT from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+router.get("/:id", verifyJWT, getMessage);
 router.post("/send/:id", verifyJWT, sendMessage);
 
 export default router;
