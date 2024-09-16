5147296d42c158e63e0e433c5ca50998090ad6ea
Mh-Monzil <monzil246@gmail.com>
2024-09-17 02:04:46 +0600

    added socket io

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes)
-app.use("/api/users", userRoutes)
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running on port ${PORT}`);
 });
